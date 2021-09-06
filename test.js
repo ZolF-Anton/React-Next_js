c3b15bf496e987f0fe8dbb7f09eace8c9c1b0c98
ZolF <zolf@bk.ru>
2021-09-07 00:18:32 +0300

    lesson_9

---
+++ test.js
@@ -0,0 +1,7 @@
+"use strict";
+var a = 5;
+var b = 4;
+var c = '4';
+var cc = true;
+// const ddd = a + cc
+var d = ['sdd', 'sdsdsf', 'sdsds', 'sdsdsd'];
---
+++ test.ts
@@ -0,0 +1,9 @@
+const a: number = 5;
+let b: number = 4;
+let c: string = '4';
+let cc = true;
+// const ddd = a + cc
+
+const d: string[] = ['sdd', 'sdsdsf', 'sdsds', 'sdsdsd'];
+
+let e: any = 3;
